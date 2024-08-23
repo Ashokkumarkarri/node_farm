f10a0c8b0905c0f067988a3d0085a4cca041d994
Ashok kumar karri <ashok.ashok1999@gmail.com>
2024-08-23 17:07:27 +0530

    implemented Routing to our app

--- index.js
+++ index.js
@@ -7,8 +7,21 @@ console.log(text)
 
 
 const server=http.createServer((req,res)=>{
-    console.log(req);
-    res.end('Hellow from the server');
+    // console.log(req);
+    // res.end('Hello from the server');
+    const pathName=req.url;
+    
+    if(pathName==='/' || pathName === '/overview'){
+        res.end('This is OverView')
+    }else if (pathName==='/product'){
+        res.end('This is Product')
+    }else{
+        res.writeHead(404,{
+            'Content-type':'text/html',
+            'my-own-header':'hellow-world'
+        });
+        res.end('<h1>Page Not found </h1>')
+    }
 })
 
 server.listen(8000,'127.0.0.1',()=>{
