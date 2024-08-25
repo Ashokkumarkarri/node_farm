db8ed9c7b6c4e06eee721fd75af1243cdb81e86c
Ashok kumar karri <ashok.ashok1999@gmail.com>
2024-08-25 23:54:32 +0530

    updated overview page with real API

--- index.js
+++ index.js
@@ -4,23 +4,56 @@ const http=require('http');
 
 
 //SERVER
-const data=fs.readFileSync('./dev-data/data.json','utf-8');
-const dataObjs=JSON.parse(data) // convert JSON string into a JavaScript object
+const repalaceTemplate=(temp,product)=>{
+    // let output=temp.replace('{%PROODUCTNAME}',product.productName)
+    let output=temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output=output.replace(/{%IMAGE%}/g,product.image)
+    output=output.replace(/{%PRICE%}/g,product.price)
+    output=output.replace(/{%NUTRIENTS%}/g,product.nutrients)
+    output=output.replace(/{%QUANTITY%}/g,product.quantity)
+    output=output.replace(/{%DESCRIPTION%}/g,product.description)
+    output=output.replace(/{%ID%}/g,product.id)
 
+    if(!product.organic) output=output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+    return output;
+}
+ 
+
+const tempOverview=fs.readFileSync(`${__dirname}/templates/overview.html`,'utf-8');
+const tempCard=fs.readFileSync(`${__dirname}/templates/card.html`,'utf-8');
+const tempProduct=fs.readFileSync(`${__dirname}/templates/product.html`,'utf-8');
+
+const data=fs.readFileSync('./dev-data/data.json','utf-8');
+const dataObj=JSON.parse(data) // convert JSON string into a JavaScript object
+console.log(dataObj)
+
+      
 const server=http.createServer((req,res)=>{
     // console.log(req);
     // res.end('Hello from the server');
     const pathName=req.url;
     
+    //Overview Page
     if(pathName==='/' || pathName === '/overview'){
-        res.end('This is OverView')
+        res.writeHead(200,{'content-type':'text/html'})
+
+        const cardsHtml = dataObj.map(el=> repalaceTemplate(tempCard,el)).join('');
+        const output =tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
+        console.log(tempCard)
+        res.end(output)
+        
+    //Product Page    
     }else if (pathName==='/product'){
         res.end('This is Product')  
+
+    
+    //API
     }else if(pathName==='/api'){
         res.writeHead(200,{'content-type':'application/json'})
         res.end(data) 
-    }
-    else{
+
+    //Not found
+    }else{
         res.writeHead(404,{
             'Content-type':'text/html',
             'my-own-header':'hellow-world'
